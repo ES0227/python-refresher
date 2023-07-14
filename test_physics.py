94a04a744b558adfadfda611c4c7fff658e9b240
ES0227 <erinsuh0227@gmail.com>
2023-07-14 15:34:42 -0400

    added new tests

--- test_physics.py
+++ test_physics.py
@@ -20,7 +20,37 @@ class testPhysics (unittest.TestCase):
         self.assertEqual(physics.calculate_pressure(5), 150375)
         with self.assertRaises(ValueError):
             (physics.calculate_pressure(-10))
+    
+    def test_acceleration_calculator (self):
+        self.assertEqual (physics.calculate_acceleration (9, 9), 1)
+        with self.assertRaises (ValueError):
+            (physics.calculate_acceleration (-1, 0))
+    
+    def test_angAcceleration_calculator (self):
+        self.assertEqual (physics.calculate_angular_acceleration (10, 5), 2)
+        with self.assertRaises (ValueError):
+            (physics.calculate_angular_acceleration (5, -3))
+    
+    def test_torque_calculator (self):
+        self.assertAlmostEqual (physics.calculate_torque (5, 45, 7), 24.749) #np.sin(np.radians(45)*35))
+        with self.assertRaises (ValueError):
+            (physics.calculate_torque (-1, 5, 4))
+
+    def test_momentOfInertia_calculator (self):
+        self.assertEqual (physics.calculate_moment_of_inertia(2, 4), 32)
+        with self.assertRaises (ValueError):
+            (physics.calculate_moment_of_inertia (-3, 5))
+    
+    def test_auvAcceleration_calculator (self):
+        self.assertAlmostEqual (physics.calculate_auv_acceleration (5, 30, 100, 0.1, 0.5))
+            
+    
+            
+            
+
+
+    
 
         
-if __name__ == "__main__":
-    __main__()
+#if __name__ == "__main__":
+    #__main__()
